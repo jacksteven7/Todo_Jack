97fdf76e1f240eca090f76c5aae4146f80057cfd
jacksteven7 <jacksteven7@gmail.com>
2015-03-09 22:30:57 -0500

    Adding items to list

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,19 @@
 class ItemsController < ApplicationController
+	before_action :set_list
+
+	def create
+		@item = @list.items.create(item_params)
+		redirect_to @list
+	end
+
+
+	private 
+
+	def set_list
+		@list = List.find(params[:list_id])
+	end
+
+	def item_params
+		params[:item].permit(:completed,:description)
+	end
 end
